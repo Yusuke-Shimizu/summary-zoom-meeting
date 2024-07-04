1f27d97b43d09753a986c26bf1d04d4dbcc32347
Yusuke Shimizu <y3-shimizu@nri-net.com>
2024-07-04 13:33:56 +0900

    mod cdk

--- summary_zoom_meeting/summary_zoom_meeting_stack.py
+++ summary_zoom_meeting/summary_zoom_meeting_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_ecs as ecs,
+    aws_ec2 as ec2,
+    aws_ecs_patterns as ecs_patterns,
 )
 from constructs import Construct
 
@@ -10,10 +11,21 @@ class SummaryZoomMeetingStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # VPCの作成
+        vpc = ec2.Vpc(self, "MyVpc", max_azs=2)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "SummaryZoomMeetingQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # ECSクラスターの作成
+        cluster = ecs.Cluster(self, "MyCluster", vpc=vpc)
+
+        # cluster: ecs.Cluster
+        ecs_patterns.ApplicationLoadBalancedFargateService(self, "Service",
+            cluster=cluster,
+            memory_limit_mib=1024,
+            cpu=512,
+            task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+                image=ecs.ContainerImage.from_asset("frontend"),
+                container_port=8501,
+                # command=["command"],
+                # entry_point=["entry", "point"]
+            )
+        )
