a819eb8ace3c3bb79cf818bba63bd1172f311616
Yusuke Shimizu <y3-shimizu@nri-net.com>
2024-07-08 18:21:08 +0900

    add ecs logs

--- summary_zoom_meeting/summary_zoom_meeting_stack.py
+++ summary_zoom_meeting/summary_zoom_meeting_stack.py
@@ -1,10 +1,12 @@
 from aws_cdk import (
     Stack,
+    Duration,
     aws_ecs as ecs,
     aws_ec2 as ec2,
     aws_ecs_patterns as ecs_patterns,
 )
 from constructs import Construct
+
 
 class SummaryZoomMeetingStack(Stack):
 
@@ -18,14 +20,24 @@ class SummaryZoomMeetingStack(Stack):
         cluster = ecs.Cluster(self, "MyCluster", vpc=vpc)
 
         # cluster: ecs.Cluster
-        ecs_patterns.ApplicationLoadBalancedFargateService(self, "Service",
+        ecs_patterns.ApplicationLoadBalancedFargateService(
+            self,
+            "Service",
             cluster=cluster,
             memory_limit_mib=1024,
             cpu=512,
             task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
                 image=ecs.ContainerImage.from_asset("frontend"),
+                container_name="frontend-container",
                 container_port=8501,
-                # command=["command"],
-                # entry_point=["entry", "point"]
-            )
+                enable_logging=True,
+                # environment={
+                #     "STREAMLIT_SERVER_PORT": "8501",
+                #     "STREAMLIT_SERVER_ENABLE_CORS": "true",  # ここを修正
+                #     "STREAMLIT_SERVER_HEADLESS": "true",
+                #     "STREAMLIT_SERVER_ADDRESS": "0.0.0.0",
+                #     "STREAMLIT_SERVER_ENABLE_XSRF_PROTECTION": "false"  # ここを追加
+                # }
+            ),
+            public_load_balancer=True,
         )
